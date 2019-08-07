cf42ae2a4ddd31612a28e0fea490a7e2a19f7a3b
sebastiaohns <sebastiaohns@gmail.com>
2019-08-06 23:13:26 -0300

    First Commit

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+
+const server = express();
+
+mongoose.connect('mongodb+srv://adminsans:adminsans@cluster0-l1zkt.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
