8aed2d36bfdb5a3e540bf68073df371835e2ef78
sebastiaohns <sebastiaohns@gmail.com>
2019-10-30 08:59:44 -0300

    updating route

--- tindev/src/routes.js
+++ tindev/src/routes.js
@@ -1,4 +1,4 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator, createDrawerNavigator, createStackNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
